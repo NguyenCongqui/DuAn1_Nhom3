92a9beeee19b9bdead8b5de5ee11c94fbe79f882
DESKTOP-VMKQ8H2\ADMIN <quincph23871@fpt.edu.vn>
2023-04-06 14:35:55 +0700

    quy

--- src/view/component/Profile.java
+++ src/view/component/Profile.java
@@ -29,9 +29,9 @@ public class Profile extends javax.swing.JPanel {
 
         jLabel1 = new javax.swing.JLabel();
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(102, 102, 255));
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/duan1/icon/12.png"))); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Trebuchet MS", 3, 36)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/duan1/icon/icons8_iPhone_14_64.png"))); // NOI18N
         jLabel1.setText("DT");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -47,7 +47,7 @@ public class Profile extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(10, 10, 10)
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 60, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
